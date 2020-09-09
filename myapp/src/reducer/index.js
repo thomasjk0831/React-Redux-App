f1ad2a17d1b92451ae7a4eedce329b4867414954
Thomas Kim <Thomasjk0831@gmail.com>
2020-09-09 12:41:53 -0400

    installed dependencies

---
+++ myapp/src/reducer/index.js
@@ -0,0 +1,10 @@
+export const initialState = {
+    movies : []
+}
+
+export const reducer = (state = initialState, action) => {
+   switch(action.type){
+       default:
+           return state
+   }
+}
